5c6ec5f9cce25a00f880f1bb84934324960b0038
Yash Contractor <yashcontractor09@gmail.com>
2024-06-23 22:43:23 -0400

    Time: 1 ms (99.82%), Space: 52.9 MB (51.17%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,19 +1,47 @@
 class Solution {
-public int majorityElement(int[] nums) {
-    int n = nums.length;
-    int majorityCount = n / 2;
+    public int majorityElement(int[] nums) {
+        // int n=nums.length;
 
-    for (int i = 0; i < n; i++) {
+        // for(int i = 0; i<n; i++) {
+        //     for(int j = i+1; j<n; j++) {
+                
+        //     }
+        // }
+
+
+        //more voting algorithm or brute force
         int count = 0;
-        for (int j = 0; j < n; j++) {
-            if (nums[j] == nums[i]) {
+        int candidate = 0;
+
+        for(int i = 0; i<nums.length; i++) {
+            if(count == 0) {
+                candidate = nums[i];
+            }
+            if(nums[i] == candidate) {
                 count++;
+            } else {
+                count--;
             }
         }
-        if (count > majorityCount) {
-            return nums[i];
-        }
+        return candidate;
+
+        //The way I was thinking about it (brute force):
+//         public int majorityElement(int[] nums) {
+//     int n = nums.length;
+//     int majorityCount = n / 2;
+
+//     for (int i = 0; i < n; i++) {
+//         int count = 0;
+//         for (int j = 0; j < n; j++) {
+//             if (nums[j] == nums[i]) {
+//                 count++;
+//             }
+//         }
+//         if (count > majorityCount) {
+//             return nums[i];
+//         }
+//     }
+//     return -1;
+// }
     }
-    return -1; // This line would never be reached if we assume there is always a majority element
-}
 }
