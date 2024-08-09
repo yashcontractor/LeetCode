6c49eefe3a50397888368382e7a7310e7c7f3e92
Yash Contractor <yashcontractor09@gmail.com>
2024-08-09 16:38:28 -0400

    Time: 2 ms (99.08%), Space: 42.6 MB (94.2%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,17 +1,25 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        
-        StringBuilder clean = new StringBuilder();
-
-        for(char c : s.toCharArray()) {
-            if(Character.isLetterOrDigit(c)) {
-                clean.append(Character.toLowerCase(c));
-            }
+        if (s.isEmpty()) {
+        	return true;
         }
-
-        String cleanedStr = clean.toString();
-        String reversedStr = clean.reverse().toString();
-
-        return cleanedStr.equals(reversedStr);
+        int start = 0;
+        int last = s.length() - 1;
+        while(start <= last) {
+        	char currFirst = s.charAt(start);
+        	char currLast = s.charAt(last);
+        	if (!Character.isLetterOrDigit(currFirst )) {
+        		start++;
+        	} else if(!Character.isLetterOrDigit(currLast)) {
+        		last--;
+        	} else {
+        		if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
+        			return false;
+        		}
+        		start++;
+        		last--;
+        	}
+        }
+        return true;
     }
 }
